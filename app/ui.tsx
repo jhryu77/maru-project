6a2bbdcd1dad466cbc5ac7db602029a136eea67f
Maru <jhryu77@gmail.com>
2025-09-06 23:16:52 +0900

    first commit

---
+++ app/ui.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Header from "@/components/header";
+
+export default function UI() {
+    return (
+        <main>
+            <Header />
+        </main>
+    );
+}
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Header() {
+    return (
+        <header className="bg-gray-800 text-white p-4 flex items-center justify-between">
+        <img src="/supanote-logo.png" alt="Supabase Note Logo" className="h-7" />
+        </header>
+    );
+    }
