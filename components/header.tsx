ccea7f6a0b5c2e8489480a129865117281223c22
Maru <jhryu77@gmail.com>
2025-09-07 16:41:24 +0900

    update

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="bg-gray-800 text-white p-4 flex items-center justify-between">
         <img src="/supanote-logo.png" alt="Supabase Note Logo" className="h-7" />
-        <h1 className="text-xl font-bold">Supanotexxx cccc</h1>
+        <h1 className="text-xl font-bold">Supanotexxx cccc,,,, master</h1>
         
         </header>
     );
